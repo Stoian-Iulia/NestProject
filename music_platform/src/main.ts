24a5838349fd035ac90f1f2e1bc945fe33e695dc
Stoian Iulia <iuliastoian8@gmail.com>
2022-11-22 12:25:59 +0200

    running server

---
+++ music_platform/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+  } catch (error) {
+    console.log(error)
+}
+  }
