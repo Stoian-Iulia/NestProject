b56aadeb73e86c444d984dd99e20a7b4b8c1524d
Stoian Iulia <iuliastoian8@gmail.com>
2022-11-22 12:44:23 +0200

    runing server

--- music_platform/src/main.ts
+++ music_platform/src/main.ts
@@ -5,8 +5,9 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
   } catch (error) {
     console.log(error)
-}
   }
+}
+start()
